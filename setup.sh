cf18c3085baed1292fc2b2570cedafa0e07dceb3
Gustavo-Kohatsu <gustavo.kohatsu@sptech.school>
2024-11-11 00:06:27 -0300

    remoção da criação do crontab automatico devido a erros no container-java

--- setup.sh
+++ setup.sh
@@ -17,27 +17,4 @@ sudo systemctl start docker
 # Rodando o Docker Compose para iniciar os containers (dentro da pasta onde o docker-compose.yml está):
 sudo docker-compose up -d
 
-# Comando utilizado pelo cron
-COMMAND="sudo docker container-java-treetech"
-
-# Define o cron timing (exemplo: rodar a cada minuto)
-CRON_TIME="* * * * *"
-
-# Verifica se o crontab já existe para o usuário
-(crontab -l 2>/dev/null) || echo "" > /tmp/crontab_backup
-
-# Verifica se o comando já está no crontab
-grep -q "$COMMAND" /tmp/crontab_backup
-if [ $? -ne 0 ]; then
-  # Se o comando não for encontrado, adiciona o cron job
-  echo "$CRON_TIME $COMMAND" >> /tmp/crontab_backup
-  crontab /tmp/crontab_backup
-  echo "Comando adicionado ao crontab com sucesso!"
-else
-  echo "Comando já existe no crontab."
-fi
-
-# Limpa o arquivo temporário
-rm /tmp/crontab_backup
-
 echo "Configuração concluída com sucesso!"
